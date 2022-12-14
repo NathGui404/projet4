87a041da49bba891a91845972e4136992533f018
Nathor <110332997+NathGui404@users.noreply.github.com>
2022-12-14 13:35:13 +0100

    Update projet4_v0.py

--- projet4_v0.py
+++ projet4_v0.py
@@ -8,30 +8,21 @@ import plotly.express as px
 import requests
 
 
-st.title('Analyse du réseau de transports en commun de la ville de Rennes')
-df_bus = pd.read_csv("df_bus_retards.csv")
+st.title('Bienvenue sur ADN Tour, votre référence tourisme en France')
+df_adn = pd.read_csv("df_bus_retards.csv")
 
-images = ['logo_star.png', 'Bus-100x100.png']
+images = ['logo_adn.png']
 st.sidebar.image(images, width=150)
 
 with st.sidebar :
-	with st.expander("Réseau Star BUS :"):
-		st.write("Lignes de bus : 152")
-		st.image("bus.jpg")
-
-option_ligne = df_bus['ligne'].unique()
+option_ligne = df_adn['schema'].unique()
 lignes = st.sidebar.selectbox(
-	'Choix ligne de bus ?',
+	"Type de point d'intérêt",
 	option_ligne
 	)
 
 # Table 
-df_bus_ligne = df_bus[df_bus['ligne']==lignes]
-st.write('Retards les plus importants par ligne de bus et par arrêt')
-df_bus_ligne=df_bus_ligne[df_bus_ligne['retard_a']=='oui'][['ligne','destination','nom_arret','arrivee_theorique','retard_arrivee']].sort_values(by='retard_arrivee',ascending=False)
-df_bus_ligne
+df_adn_poi = df_adn[df_adn['schema']==lignes]
+st.write('Liste des',lignes)
 
-df_bus_tard = df_bus[(df_bus['ligne']==lignes)&(df_bus['retard_a']=='oui')]
-st.write('Retards supérieurs à 3 minutes')
-st.map(df_bus_tard[['latitude','longitude']])
 
